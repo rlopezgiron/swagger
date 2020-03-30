d80655d7d53a4e4769aae5d69ec3dd45bb3779ad
López Girón <roberto.lopez.giron@accenture.com>
2020-03-30 14:23:44 +0200

    added contact info

--- src/main/java/com/test/swagger/config/SwaggerConfig.java
+++ src/main/java/com/test/swagger/config/SwaggerConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.fasterxml.classmate.TypeResolver;
 
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -23,6 +24,7 @@ import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.schema.WildcardType;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -95,6 +97,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .version("1.0.0")
                 .license("Apache License Version 2.0")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Roberto Lopez Giron", "", "roberto.lopez.giron@gmail.com"))
                 .build();
     }
 	
